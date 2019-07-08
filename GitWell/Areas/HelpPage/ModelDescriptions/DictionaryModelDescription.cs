fc6e17659e698aa0ac768d3e18b05f7a18a1b5b0
csa2006 <csa2006@hotmail.com>
2019-07-08 10:48:17 +0800

    添加项目文件。

---
+++ GitWell/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GitWell.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GitWell/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitWell.WebApiApplication" Language="C#" %>
