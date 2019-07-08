085116dc1ec6814d66a2389d89e8d9af52b30a61
csa2006 <csa2006@hotmail.com>
2019-07-08 12:39:44 +0800

    新建了实体数据模型项目EDM

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29f1597b-1235-4849-aca9-76f1beb5d35d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EDM;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using(IMS_SZEntities db = new IMS_SZEntities())
+            {
+                var query = db.CRM_Customer.AsNoTracking().Select(x => new
+                {
+                    Code = x.Code,
+                    Name = x.Name
+                }).ToList();
+            }
+        }
+    }
+}
